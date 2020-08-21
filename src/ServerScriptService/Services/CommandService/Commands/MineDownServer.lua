c3db32fac39718913940e67375e79c0049f9e495
sam <sayhisam1@gmail.com>
2020-08-20 23:24:32 -0400

    Minedown Fixed

--- src/ServerScriptService/Services/CommandService/Commands/MineDownServer.lua
+++ src/ServerScriptService/Services/CommandService/Commands/MineDownServer.lua
@@ -1,16 +1,14 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Services = require(ReplicatedStorage.Services)
-local Map = Services.Map
+local OreBinder = require(ReplicatedStorage.OreBinder)
 
 local BLOCK_SIZE = 7
 return function(context, depth)
 	local plr = context.Executor
 	local pos = plr.Character.PrimaryPart.Position
-	local quarry = Map:GetQuarry()
 
 	for i = 1, depth, 1 do
 		local blockPos = pos - Vector3.new(0, i * BLOCK_SIZE, 0)
-		local ore = quarry:GetBlockAtAbsoluteCoordinates(blockPos.X, blockPos.Y, blockPos.Z)
+		local ore = OreBinder:GetNearestOreNeighbor(blockPos)
 		if ore then
 			ore:Destroy()
 		end
