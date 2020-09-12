c3ec135d7d976d844a91439dee1fd9dc0e117f5a
sam <sayhisam1@gmail.com>
2020-09-11 20:03:41 -0400

    waitforchild: Change promise to fix sync issues

--- src/ReplicatedStorage/Objects/Promises/WaitForChildPromise.lua
+++ src/ReplicatedStorage/Objects/Promises/WaitForChildPromise.lua
@@ -3,22 +3,6 @@ local Promise = require(ReplicatedStorage.Lib.Promise)
 
 return function(instance, child)
     return Promise.new(function(resolve, _, onCancel)
-        local found = instance:FindFirstChild(child)
-        if not found then
-            local event
-            event = instance.DescendantAdded:Connect(function(descendant)
-                if descendant.Name == child then
-                    event:Disconnect()
-                    resolve(descendant)
-                end
-            end)
-            onCancel(
-                function()
-                    event:Disconnect()
-                end
-            )
-        else
-            resolve(found)
-        end
+        resolve(instance:WaitForChild(child))
     end)
 end
