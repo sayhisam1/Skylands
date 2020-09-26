52d24c4fe5209d74f38e3217d2a25a0cdda583a1
sam <sayhisam1@gmail.com>
2020-09-26 04:30:12 -0400

    formatting + add robux path

--- src/ReplicatedStorage/Binders/PetDispenserBinder/ServerPetDispenserSetup.lua
+++ src/ReplicatedStorage/Binders/PetDispenserBinder/ServerPetDispenserSetup.lua
@@ -2,6 +2,7 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local RunService = game:GetService("RunService")
 local Services = require(ReplicatedStorage.Services)
 local EffectsService = Services.EffectsService
+local MarketplaceHandler = Services.MarketplaceHandler
 
 local TableUtil = require(ReplicatedStorage.Utils.TableUtil)
 return function(dispenser)
@@ -15,17 +16,23 @@ return function(dispenser)
 			function(plr)
 				dispenser:TryPurchase(plr, 1):andThenCall(dispenser.RollPet, dispenser, plr, 1):andThen(
 					function(pets)
-						pets = TableUtil.map(pets, function(_, pet)
-							local petInst = pet:GetInstance():Clone()
-							EffectsService:AddTemporarySharedInstance(petInst, 10)
-							return petInst
-						end)
+						pets =
+							TableUtil.map(
+							pets,
+							function(_, pet)
+								local petInst = pet:GetInstance():Clone()
+								EffectsService:AddTemporarySharedInstance(petInst, 10)
+								return petInst
+							end
+						)
 						nc:PublishPlayer(plr, "BOUGHT_PETS", pets)
 					end
-				):catch(function(err)
-					dispenser:Log(3, plr, "failed with error", err.code)
-					nc:PublishPlayer(plr, "ERROR", err.code)
-				end)
+				):catch(
+					function(err)
+						dispenser:Log(3, plr, "failed with error", err.code)
+						nc:PublishPlayer(plr, "ERROR", err.code)
+					end
+				)
 			end
 		)
 	)
@@ -36,18 +43,52 @@ return function(dispenser)
 			function(plr)
 				dispenser:TryPurchase(plr, 3):andThenCall(dispenser.RollPet, dispenser, plr, 3):andThen(
 					function(pets)
-						pets = TableUtil.map(pets, function(_, pet)
-							local petInst = pet:GetInstance():Clone()
-							EffectsService:AddTemporarySharedInstance(petInst, 10)
-							return petInst
-						end)
+						pets =
+							TableUtil.map(
+							pets,
+							function(_, pet)
+								local petInst = pet:GetInstance():Clone()
+								EffectsService:AddTemporarySharedInstance(petInst, 10)
+								return petInst
+							end
+						)
 						nc:PublishPlayer(plr, "BOUGHT_PETS", pets)
 					end
-				):catch(function(err)
-					dispenser:Log(3, plr, "failed with error", err.code)
-					nc:PublishPlayer(plr, "ERROR", err.code)
-				end)
+				):catch(
+					function(err)
+						dispenser:Log(3, plr, "failed with error", err.code)
+						nc:PublishPlayer(plr, "ERROR", err.code)
+					end
+				)
 			end
 		)
 	)
+
+	if dispenser:GetAttribute("DevproductId") then
+		local id = dispenser:GetAttribute("DevproductId")
+		MarketplaceHandler:RegisterHandler(
+			id,
+			function(plr)
+				return dispenser:RollPet(plr, 1):andThen(
+					function(pets)
+						pets =
+							TableUtil.map(
+							pets,
+							function(_, pet)
+								local petInst = pet:GetInstance():Clone()
+								EffectsService:AddTemporarySharedInstance(petInst, 10)
+								return petInst
+							end
+						)
+						nc:PublishPlayer(plr, "BOUGHT_PETS", pets)
+					end
+				):catch(
+					function(err)
+						dispenser:Log(3, plr, "failed with error", err.code)
+						nc:PublishPlayer(plr, "ERROR", err.code)
+					end
+				):expect()
+			end
+		)
+	end
 end
